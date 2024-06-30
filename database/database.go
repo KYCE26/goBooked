0b81ab77944f193a6872df9da8b00e375237d10e
MUHAMMAD RIFKY <muhammad26rifky06@gmail.com>
2024-06-30 12:28:59 +0700

    first

--- database/database.go
+++ database/database.go
@@ -1,8 +1,8 @@
 package database
 
 import (
-    "database/sql"
-    "log"
+	"database/sql"
+	"log"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -10,17 +10,17 @@ import (
 var DB *sql.DB
 
 func InitDB() {
-    var err error
-    connStr := "root@tcp(localhost:3306)/go_booked"
-    DB, err = sql.Open("mysql", connStr)
-    if err != nil {
-        log.Fatal(err)
-    }
+	var err error
+	connStr := "root:p3ws@tcp(localhost:3306)/go_booked"
+	DB, err = sql.Open("mysql", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    err = DB.Ping()
-    if err != nil {
-        log.Fatal(err)
-    }
+	err = DB.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    log.Println("Database connected")
+	log.Println("Database connected")
 }
